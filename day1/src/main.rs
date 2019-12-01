13791a0769e451fe97b1e7b13a1c85e388359e7e
seve.hooker@gmail.com <seve.hooker@gmail.com>
2019-12-01 16:53:58 -0600

    day1 done

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,31 +1,70 @@
 use std::fs::File;
 use std::io::{BufReader,BufRead};
 
+// struct Fuel {
+//     amount: i32
+// }
+
+// impl Iterator for Fuel {
+//     type Item = i32;
+
+//     fn next(&mut self) -> Option<Self::Item> {
+//         let mut total = 0;
+//         let mut cur_amount = self.amount;
+
+//         while cur_amount > 1 {
+//             cur_amount = (cur_amount/3)-2;
+//             total += cur_amount;
+//         }
+
+//         Some(total);
+//     }
+// }
+
 fn main() {
-    let file = File::open("resource/mass.txt").unwrap();
+  let file = File::open("resource/mass.txt").unwrap();
+  let mut numbers = Vec::new();
+
+  let total_fuel = move |sum:i32, fuel: &i32| {
+    let mut fuel_fuel = *fuel;
+    let mut total = 0;
+
+    while fuel_fuel > 8 {
+      fuel_fuel = (fuel_fuel/3)-2;
+      total += fuel_fuel;
+    }
+
+    return sum + total;
+  };
+
+  for line in BufReader::new(file).lines() {
+    numbers.push(line.unwrap().parse::<i32>().unwrap());
+  }
+  
+  println!("{}", numbers.iter().fold(0, total_fuel));
+}
+
+#[cfg(test)]
+mod tests {
+  #[test]
+  fn test_day1() {
     let mut numbers = Vec::new();
+    numbers.push(1969);
 
-    for line in BufReader::new(file).lines() {
-        numbers.push(line.unwrap().parse::<i32>().unwrap());
-    }
-    
-    println!("{}", numbers.iter().fold(0, |sum, fuel| sum + ((fuel/3)-2)));
-    println!("{}", part_1(&numbers));
-    // println!("{}", part_2(&numbers));
-}
+    let total_fuel = move |sum:i32, mass: &i32| {
+      let mut fuel_mass = *mass;
+      let mut total = 0;
+  
+      while fuel_mass > 8 {
+        fuel_mass = (fuel_mass/3)-2;
+        println!("{}", fuel_mass);
 
-fn part_1(numbers: &[i32]) -> i32 {
-    let mut sum = 0;
+        total += fuel_mass;
+      }
+  
+      return sum + total;
+    };
 
-    for mass in numbers {
-        let fuel = mass / 3;
-        sum += fuel - 2;
-    }
-
-    return sum;
-}
-
-fn part_2(numbers: &[i32]) -> i32 {
-    let mut sum = 0;
-
+    assert_eq!(numbers.iter().fold(0, total_fuel), 966);
+  }
 }
